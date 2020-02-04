ca7a60febb80d38078be3ac77475494063a8c0cb
gopikrishna-p <gopikrishna.pavuluri1996@gmail.com>
2020-02-04 23:51:45 +0530

    Updates

---
+++ precache-manifest.6737687bdc689b4b93e641e855f8d531.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4a1be2f028a6d2a5880d42afb1a62c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8084a5d7f7bb7d918244",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b8ffb99ba29b60aa1511",
+    "url": "/static/js/2.8d5f3e0f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.8d5f3e0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8084a5d7f7bb7d918244",
+    "url": "/static/js/main.bca2fc73.chunk.js"
+  },
+  {
+    "revision": "b32f6bd80fab9d01d47e",
+    "url": "/static/js/runtime-main.7a7bae4c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
